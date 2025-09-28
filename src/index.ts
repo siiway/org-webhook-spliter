293deb432b5cdca98ac367baada9cc83913cdc36
wyf9 <wyf9@wyf9.top>
2025-09-29 06:25:36 +0800

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Webhooks } from '@octokit/webhooks'
+import { Env } from './utils'
+
+
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env: Env, ctx): Promise<Response> {
+		const url = new URL(request.url).pathname
+		console.log(`Request: ${url}`)
+		if (url === '/') {
+			return new Response('', { status: 301, headers: { 'Location': 'about:blank' } })
+		}
+
+		if (request.method !== 'POST') {
+			return new Response('Method not allowed', { status: 405 })
+		}
+
+		const signature = request.headers.get('X-Hub-Signature-256') || 'None'
+		const body = await request.text()
+		const secret = env.SECRET
+		const webhook = new Webhooks({
+			secret
+		});
+
+		if (!webhook.verify(body, signature)) {
+			return new Response('Signature verify failed!', { status: 401 })
+		}
+		return new Response('Hello World!')
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
